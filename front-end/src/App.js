57ad758c42bdff8ce6a08928ccc588812858d95d
Musaddiqur Rahman <mashrur749@gmail.com>
2020-12-15 18:30:00 -0500

    front end initiated

---
+++ front-end/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from "./components/Login.js"
+import Register from "./components/Register"
+import NotFound from "./components/NotFound"
+
+function App() {
+  return (
+    <div className="App">  
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" render={() => <div>Main Page</div>} exact/>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
