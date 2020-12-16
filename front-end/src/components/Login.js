7e21e7d97d95ca592220e536ade1e683a8176dbb
Musaddiqur Rahman <mashrur749@gmail.com>
2020-12-15 19:09:31 -0500

    form padding fixed

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -7,26 +7,29 @@ const Login = () => {
     return (
         <div>
             <h1> Login </h1>
-            <Form>
-                <Form.Group as={Row} controlId="formPlaintextEmail">
-                <Form.Label column sm="2">
-                    Username
-                </Form.Label>
-                <Col sm="4">
-                    <Form.Control placeholder="Username" />
-                </Col>
+            <br/>
+            <Row className="justify-content-md-center">
+                <Form>
+                    <Form.Group as={Row} controlId="formPlaintextEmail">
+                    <Form.Label column sm="4">
+                        Username
+                    </Form.Label>
+                    <Col sm="8">
+                        <Form.Control placeholder="Username" />
+                    </Col>
+                    
+                    </Form.Group>
                 
-                </Form.Group>
-            
-                <Form.Group as={Row} controlId="formPlaintextPassword">
-                <Form.Label column sm="2">
-                    Password
-                </Form.Label>
-                <Col sm="4">
-                    <Form.Control type="password" placeholder="Password" />
-                </Col>
-                </Form.Group>
-            </Form>
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                    <Form.Label column sm="4">
+                        Password
+                    </Form.Label>
+                    <Col sm="8">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                    </Form.Group>
+                </Form>
+            </Row>
         </div>
     );
 }
--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -12,33 +12,36 @@ const Register = () => {
     return (
         <div>
             <h1> Register </h1>
-            <Form>
-            
-                <Form.Group as={Row} controlId="formPlaintextEmail">
-                <Form.Label column sm="2">
-                    Username
-                </Form.Label>
-                <Col sm="2">
-                    <Form.Control placeholder="Username" />
-                </Col>
-                <Col sm="1">
-                    <DropdownButton id="dropdown-basic-button" title="Account Type">
-                        <Dropdown.Item value="1">Employee</Dropdown.Item>
-                        <Dropdown.Item value="2">Customer</Dropdown.Item>
-                    </DropdownButton>
-                </Col>
-                </Form.Group>
-            
-                <Form.Group as={Row} controlId="formPlaintextPassword">
-                <Form.Label column sm="2">
-                    Password
-                </Form.Label>
-                <Col sm="2">
-                    <Form.Control type="password" placeholder="Password" />
-                </Col>
-                </Form.Group>
+            <br/>
+            <Row className="justify-content-md-center">
+                <Form>
+                
+                    <Form.Group as={Row} controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">
+                        Username
+                    </Form.Label>
+                    <Col sm="8">
+                        <Form.Control placeholder="Username" />
+                    </Col>
+                    <Col sm="1">
+                        <DropdownButton id="dropdown-basic-button" title="Account Type">
+                            <Dropdown.Item value="1">Employee</Dropdown.Item>
+                            <Dropdown.Item value="2">Customer</Dropdown.Item>
+                        </DropdownButton>
+                    </Col>
+                    </Form.Group>
+                
+                    <Form.Group as={Row} controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                        Password
+                    </Form.Label>
+                    <Col sm="8">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                    </Form.Group>
 
-            </Form>
+                </Form>
+            </Row>
         </div>
     );
 }
